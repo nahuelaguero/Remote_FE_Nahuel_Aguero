7464aa41997d7182e2892b4826d5a7c8133a7623
Nahuel Aguero <nahuelaguero@Nahuels-MacBook-Pro.local>
2024-06-09 13:30:35 -0400

    Prepare for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+  };
+  
+  module.exports = nextConfig;
